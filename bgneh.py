8d634c8db4fe953b9f9688dd309426e942356698
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2022-07-24 23:10:39 +0200

    Add Straetgey pattern
    now, we have a `scraper` class that should contain the shared logic
    between shops where you can dynamically alter the shop in the scarper.

--- bgneh.py
+++ bgneh.py
@@ -1,45 +1,31 @@
+import contextlib
 from operator import itemgetter
 from unittest import result
 import requests
 from bs4 import BeautifulSoup
+from typing import List, Dict, Tuple, Callable
 
 
-# def gammal_search(query: str):
-#     elgammal_search_url = "http://www.elgammalelectronics.com/Home/SearchResults?TheSearchParameter="
-#     r = requests.get(elgammal_search_url + query)
-#     soup = BeautifulSoup(r.text, 'html.parser')
-#     data = []
-#     table = soup.find('table', attrs={'class':'gridtable'})
-#     rows = table.find_all('tr')
-#     for row in rows:
-#         cols = row.find_all('td')
-#         cols = [ele.text.strip() for ele in cols]
-#         data.append([ele for ele in cols if ele]) # Get rid of empty values
-#     # returns a list of lists
-#     # each list is of the format ['ITEM_NAME', 'ITEM_DESC', 'PRICE_NUM', 'CURRENCY']
-#     return data
-# 
 def gammal_search(query: str):
     elgammal_search_url = "http://www.elgammalelectronics.com/Home/SearchResults?TheSearchParameter="
     r = requests.get(elgammal_search_url + query)
     soup = BeautifulSoup(r.text, 'html.parser')
     data = []
-    table = soup.find('table', attrs={'class':'gridtable'})
+    table = soup.find('table', attrs={'class': 'gridtable'})
     items = table.find_all('tr')
     for item in items:
-        try:
+        with contextlib.suppress(Exception):
             cols = item.find_all('td')
             cols = [ele.text.strip() for ele in cols]
-            link = item.find(attrs={'style':'cursor:pointer;'}).get('id')
+            link = item.find(attrs={'style': 'cursor:pointer;'}).get('id')
             link = link.split("-")[1:]
             link = "-".join(link)
-            link = "http://www.elgammalelectronics.com/Products/Details/" + link
+            link = f"http://www.elgammalelectronics.com/Products/Details/{link}"
             cols.append(link)
             data.append([ele for ele in cols if ele])
-        except:
-            pass
     data = [item for item in data if item]
-    data = [[f"{item[0]} {item[1]}", float(item[2]), item[-1]] for item in data]
+    data = [[f"{item[0]} {item[1]}", float(
+        item[2]), item[-1]] for item in data]
     data.sort(key=itemgetter(1))
     return data
 
@@ -49,11 +35,12 @@ def fut_search(query: str):
     r = requests.get(fut_search_url)
     soup = BeautifulSoup(r.text, 'html.parser')
     try:
-        no_of_pages = soup.find_all('div', id="paging")[0].find_all('a')[-2].text
-    except:
+        no_of_pages = soup.find_all('div', id="paging")[
+            0].find_all('a')[-2].text
+    except Exception:
         no_of_pages = 1
     data = []
-    for page in range(1, int(no_of_pages) + 1):
+    for page in range(1, no_of_pages + 1):
         fut_search_url = f"https://store.fut-electronics.com/search?page={page}&q={query}"
         r = requests.get(fut_search_url)
         soup = BeautifulSoup(r.text, 'html.parser')
@@ -63,12 +50,14 @@ def fut_search(query: str):
             item_price = item.findChildren("h4")[0].text
             item_price = item_price.strip("LE ").replace(",", "")
             link = item.findChildren("a")[0].get('href')
-            link = "https://store.fut-electronics.com" + link
+            link = f"https://store.fut-electronics.com{link}"
             data.append([item_name, float(item_price), link])
     data.sort(key=itemgetter(1))
     return data
 
 # Test this more cuz it seems to break often
+
+
 def ram_search(query: str):
     params = {
         's': query,
@@ -85,13 +74,16 @@ def ram_search(query: str):
     r = requests.post('https://ram-e-shop.com/', params=params, data=data)
     soup = BeautifulSoup(r.text, 'html.parser')
     data = []
-    results = soup.find('ul', attrs={'class':'products columns-4 columns__wide--5'})
+    results = soup.find(
+        'ul', attrs={'class': 'products columns-4 columns__wide--5'})
     results = results.find_all('li')
     for result in results:
-        title = result.find('h2', attrs={'class':'woocommerce-loop-product__title'}).text
+        title = result.find(
+            'h2', attrs={'class': 'woocommerce-loop-product__title'}).text
         price = result.find('bdi').text
         data.append([title, price])
     return data
+
 
 def free_search(query: str):
     params = {
@@ -109,13 +101,51 @@ def free_search(query: str):
     r = requests.post('https://free-electronic.com/', params=params, data=data)
     soup = BeautifulSoup(r.text, 'html.parser')
     data = []
-    results = soup.find('ul', attrs={'class':'products columns-4 columns__wide--5'})
+    results = soup.find(
+        'ul', attrs={'class': 'products columns-4 columns__wide--5'})
     results = results.find_all('li')
     for result in results:
-        title = result.find('h2', attrs={'class':'woocommerce-loop-product__title'}).text
-        price = result.find('span', attrs={'class':'woocommerce-Price-amount amount'}).text
+        title = result.find(
+            'h2', attrs={'class': 'woocommerce-loop-product__title'}).text
+        price = result.find(
+            'span', attrs={'class': 'woocommerce-Price-amount amount'}).text
         price = price.strip(u'\xa0EGP')
         data.append([title, price])
     return data
 
-print(ram_search("ic"))
+
+SearchEngineFunction = Callable[str, List[Tuple[str, float]]]
+
+
+class Scraper():
+
+    def __init__(self, shop_engine, query: str):
+        self.shop_engine = shop_engine
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+        }
+        self.query = query
+
+    def run(self) -> List[Tuple[str, float]]:
+        return self.shop_engine(self.query)
+
+    def save_to_file(self, filename: str) -> None:
+        with open(filename, 'w') as f:
+            for item in self.run():
+                f.write(f"{item[0]} {item[1]}\n")
+
+
+def main() -> None:
+
+    shop_engines = [
+        (gammal_search, "Gammal Electronics"),
+        (fut_search, "Fut Electronics"),
+        (ram_search, "Ram-E-Shop"),
+        (free_search, "Free Electronics"),
+    ]
+    scarper = Scraper(shop_engine=free_search, query="ic")
+    data = scarper.run()
+    print(data)
+
+
+main()
